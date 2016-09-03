e55d3ba46b6c2b924624c548d586826e4ae280b3
Santiago Garza <santiago.garza@imforza.com>
2016-09-02 18:46:58 -0700

    started defining methods

--- wp-uptime-robot-api.php
+++ wp-uptime-robot-api.php
@@ -14,4 +14,83 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
  */
 class UptimeRobotApi {
 
+	/**
+	 * [__construct description]
+	 * @param [type] $api_key [description]
+	 */
+	public function __construct( $api_key = null ) {
+
+	}
+
+	/**
+	 * [get_account_details description]
+	 * @return [type] [description]
+	 */
+	protected function get_account_details(){
+
+	}
+
+	/**
+	 * [get_monitors description]
+	 * @return [type] [description]
+	 */
+	protected function get_monitors(){
+
+	}
+
+	/**
+	 * [new_monitor description]
+	 * @return [type] [description]
+	 */
+	protected function new_monitor(){
+
+	}
+
+	/**
+	 * [edit_monitor description]
+	 * @return [type] [description]
+	 */
+	protected function edit_monitor(){
+
+	}
+
+	/**
+	 * [delete_monitor description]
+	 * @return [type] [description]
+	 */
+	protected function delete_monitor(){
+
+	}
+
+	/**
+	 * [reset_monitor description]
+	 */
+	protected function reset_monitor(){
+
+	}
+
+	/**
+	 * [get_alert_contacts description]
+	 * @return [type] [description]
+	 */
+	protected function get_alert_contacts(){
+
+	}
+
+	/**
+	 * [new_alert_contact description]
+	 * @return [type] [description]
+	 */
+	protected function new_alert_contact(){
+
+	}
+
+	/**
+	 * [delete_alert_contact description]
+	 * @return [type] [description]
+	 */
+	protected function delete_alert_contact(){
+
+	}
+
 }
