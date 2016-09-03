b29e1cd6e7a04dfd71721c0e90ed82aa704bbf84
Brandon Hubbard <bkhubbard@gmail.com>
2016-09-02 17:10:45 -0700

    started file

---
+++ wp-uptime-robot-api.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * WP-UptimeRobot-API
+ *
+ * @package WP-UptimeRobot-API
+ */
+
+/* Exit if accessed directly */
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+
+/**
+ * WP_UptimeRobot_API class.
+ */
+class WP_UptimeRobot_API {
+
+}
