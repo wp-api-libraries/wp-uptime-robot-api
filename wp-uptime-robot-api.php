c61ab017f3ad2034b33bc4efc2564f8e2e65a6d2
Santiago Garza <santiago.garza@imforza.com>
2016-09-08 19:32:59 -0700

    codeclimate fixes

--- wp-uptime-robot-api.php
+++ wp-uptime-robot-api.php
@@ -105,16 +105,16 @@ class UptimeRobotApi {
 	 * @param  [type]   $custom_up_ratio       [description].
 	 * @param  [Int]    $logs                  [description].
 	 * @param  [Int]    $response_times        [description].
-	 * @param  [Int]    $response_time_average [description].
+	 * @param  [Int]    $response_average [description].
 	 * @param  [Int]    $alert_contacts        [description].
 	 * @param  [Int]    $show_alert_contacts   [description].
 	 * @param  [Int]    $show_timezone         [description].
 	 * @return [type]                          [description]
 	 */
-	public function get_monitors( $search = '', $monitors = null, $custom_up_ratio = null, $logs = 0, $response_times = 0, $response_time_average = 0, $alert_contacts = 0, $show_alert_contacts = 0, $show_timezone = 0 ) {
+	public function get_monitors( $search = '', $monitors = null, $custom_up_ratio = null, $logs = 0, $response_times = 0, $response_average = 0, $alert_contacts = 0, $show_alert_contacts = 0, $show_timezone = 0 ) {
 		$request = $this->base_uri . '/getMonitors';
 
-		$request .= '?logs=' . $logs . '&responseTimes=' . $response_times . '&responseTimesAverage=' . $response_time_average . '&alertContacts=' . $alert_contacts . '&showMonitorAlertContacts=' . $show_alert_contacts . '&showTimezone=' . $show_timezone;
+		$request .= '?logs=' . $logs . '&responseTimes=' . $response_times . '&responseTimesAverage=' . $response_average . '&alertContacts=' . $alert_contacts . '&showMonitorAlertContacts=' . $show_alert_contacts . '&showTimezone=' . $show_timezone;
 
 		if ( ! empty( $search ) ) {
 			$request .= '&search=' . htmlspecialchars( $search );
@@ -233,7 +233,7 @@ class UptimeRobotApi {
 	public function get_alert_contacts( $alert_contacts = null ) {
 		$request = $this->base_uri . '/getAlertContacts';
 
-		if ( ! empty( $alertcontacts ) ) {
+		if ( ! empty( $alert_contacts ) ) {
 			$request .= '?alertcontacts=' . $this->get_implode( $alert_contacts );
 		}
 
